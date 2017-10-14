17d72d4c0a3043cc04a786ccdc2f80452db76d45
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-14 16:20:07 -0500

    Get document by query selector

---
+++ main.js
@@ -0,0 +1,5 @@
+//Get document by querySelector from my HTML file
+let container = document.querySelector('.container');
+let container1 = document.querySelector('.container1');
+let container2 = document.querySelector('.container2');
+let container3 = document.querySelector('.container3');

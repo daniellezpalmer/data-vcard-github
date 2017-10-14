915a10cff5e60e1a59a499a93785cf5b6f0b66b3
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-14 16:20:33 -0500

    Fetch user data from github api

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ let container = document.querySelector('.container');
 let container1 = document.querySelector('.container1');
 let container2 = document.querySelector('.container2');
 let container3 = document.querySelector('.container3');
+
+fetch('https://api.github.com/users/daniellezpalmer')
+.then(function(response) {
+  return response.json()
+}).catch(function(err) {
+ console.log('error');
+})
+.then(function(data) {
+  // console.log(data);
+  console.log(data.name);
+  header(data);
+  basics(data);
+  story(data);
+  image(data);
+})
